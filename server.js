d157b52c42862802bd6115abbabc29b0c5e429a1
ajit-drag <ajit.drag@gmail.com>
2016-12-25 01:11:24 +0530

    Added single dialog

--- server.js
+++ server.js
@@ -12,18 +12,15 @@ var chatConnector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(chatConnector);
-var nlpModel = "https://api.projectoxford.ai/luis/v1/application?id=5473dc3e-47b9-4d60-9f4f-3f876653946e&subscription-key=7d28d1d8006a4a2b8cf7945c316e8116&q=";
-var recognizer = new builder.LuisRecognizer(nlpModel);
-var intent = new builder.IntentDialog({ recognizers: [recognizer] });
 
-bot.dialog('/', intent);
+bot.dialog('/', function(session) {
+    session.send("Hello Creator !!");
+});
 
-intent.matches('greeting', function (session) { greeting(session,builder) });
-intent.onDefault(builder.DialogAction.send("I am not saying anything about this !!"));
 
 server.post('/api/messages', chatConnector.listen());
 
 server.use('/', express.static('docs'));
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log('Server is listening..');
 });
