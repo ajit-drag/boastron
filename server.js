77f0ada7127c84479e88d5ffe7bb6fc48c375b89
ajit-drag <ajit.drag@gmail.com>
2016-12-25 12:35:34 +0530

    Added LUIS

--- server.js
+++ server.js
@@ -13,14 +13,22 @@ var chatConnector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(chatConnector);
-var nlpModel = "https://api.projectoxford.ai/luis/v1/application?id=5473dc3e-47b9-4d60-9f4f-3f876653946e&subscription-key=7d28d1d8006a4a2b8cf7945c316e8116&q=";
+
+var nlpModel = "https://api.projectoxford.ai/luis/v2.0/apps/5473dc3e-47b9-4d60-9f4f-3f876653946e?subscription-key=7d28d1d8006a4a2b8cf7945c316e8116&verbose=true";
 var recognizer = new builder.LuisRecognizer(nlpModel);
 var intent = new builder.IntentDialog({ recognizers: [recognizer] });
 
 bot.dialog('/', intent);
 
-intent.matches('greeting', function(session) { greeting(session, builder) });
-intent.matches('introduction', function(session) { introduction(session, builder) });
+// intent.matches('greeting', function(session) { greeting(session, builder) });
+// intent.matches('introduction', function(session) { introduction(session, builder) });
+intent.matches('greeting', function(session) {
+    session.send("Hello User !!");
+});
+
+intent.matches('introduction', function(session) {
+    session.send("My Name is Boastron !!");
+});
 intent.onDefault(builder.DialogAction.send("Sorry but sometime I don't know what you want and this is that exact moment !!"));
 
 server.post('/api/messages', chatConnector.listen());
