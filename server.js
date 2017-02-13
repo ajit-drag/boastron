f75417e29c7435a75083d49ed2dd094a77e7ca70
ajit-drag <ajit.drag@gmail.com>
2017-02-14 00:08:30 +0530

    Logging

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var router = require('./route.js');
 
 var server = express();
 
+server.use(function(req, res, next) {
+    console.log("req", req);
+    console.log("res", res);
+    next();
+});
 server.use('/', router);
 server.use('/', express.static('site'));
 
